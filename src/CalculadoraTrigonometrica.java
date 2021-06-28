9d8adb1f86fca722081f4ce207f47f610c73d134
KendallMM <kendallmarinmunoz@gmail.com>
2021-06-27 21:23:24 -0600

    Definí el método de entrada para la calculadora.

--- src/CalculadoraTrigonometrica.java
+++ src/CalculadoraTrigonometrica.java
@@ -12,6 +12,18 @@ public class CalculadoraTrigonometrica {
         System.out.println("Calcular tangente hiperbólico = Escriba tanh");
         System.out.println("El separador decimal es la coma");
 
+        System.out.println("Escriba debajo de este texto la operación que desea realizar de acuerdo a los parámetros dados");
+        Scanner operacion = new Scanner(System.in);
+        String calcular = operacion.nextLine();
+        System.out.println("Usted ha elegido realizar la operación trigonométrica:   " + calcular);
+
+        System.out.println("Escriba debajo de este texto el ángulo que desea calcular");
+
+        Scanner numero = new Scanner(System.in);
+
+        double anguloEnGrados = numero.nextDouble();
+        double resultado = 0;
+        System.out.println("El ángulo que usted escribió corresponde a:   " + anguloEnGrados + "º");
 
     }
 }
