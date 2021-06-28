74e1cae558d28f6b743bb4476597e544a59f4adf
goz310103 <tamiguisimo@gmail.com>
2021-06-27 21:21:15 -0600

    José A:  Escribí los primeros print que guiarán al usuario en el  uso de la calculadora trigonométrica

--- src/CalculadoraTrigonometrica.java
+++ src/CalculadoraTrigonometrica.java
@@ -1,8 +1,17 @@
 import java.util.Scanner;
 public class CalculadoraTrigonometrica {
     public static void main(String[] args) {
+        System.out.println("Calcular seno = Escriba sin");
+        System.out.println("Calcular coseno = Escriba cos");
+        System.out.println("Calcular tangente = Escriba tan");
+        System.out.println("Calcular arcoseno = Escriba arcsin");
+        System.out.println("Calcular arcocoseno = Escriba arccos");
+        System.out.println("Calcular arcotangente = Escriba arctan");
+        System.out.println("Calcular seno hiperbólico = Escriba sinh");
+        System.out.println("Calcular coseno hiperbólico = Escriba cosh");
+        System.out.println("Calcular tangente hiperbólico = Escriba tanh");
+        System.out.println("El separador decimal es la coma");
 
-        System.out.println("Hola Mundo!");
-        //prueba commit
+
     }
 }
